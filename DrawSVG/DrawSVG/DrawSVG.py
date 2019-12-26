c9466c743d7709ccc4bcd15fe5084e5dca927905
user <user@PL1CNSHA0202NB.net.plm.eds.com>
2019-12-26 12:42:45 +0800

    flip y-axis

--- DrawSVG/DrawSVG/DrawSVG.py
+++ DrawSVG/DrawSVG/DrawSVG.py
@@ -17,7 +17,7 @@ def line(prop,pos=None):
     line_start[1]=MAX_HEIGHT-(float(line_start[1])+float(line_ref[1]))
     line_end[0]=float(line_end[0])+float(line_ref[0])
     line_end[1]=MAX_HEIGHT-(float(line_end[1])+float(line_ref[1]))
-    svg_entity = svgwrite.Drawing().line(start=tuple(line_start), end=tuple(line_end), stroke = "black", stroke_width = 1.0/SCALE )
+    svg_entity = svgwrite.Drawing().line(start=tuple(line_start), end=tuple(line_end), stroke = "black", stroke_width = 0.5/SCALE )
     #svg_entity.scale(SCALE,-SCALE)
     return svg_entity
 def circle(prop,pos=None):
@@ -27,21 +27,21 @@ def circle(prop,pos=None):
     circle_center = list(prop["Position"]["Location"].values())[:2]
     circle_radius = float(prop["@Radius"])
     circle_center[0]=float(circle_center[0])+float(circle_ref[0])
-    circle_center[1]=float(circle_center[1])+float(circle_ref[1])
-    svg_entity = svgwrite.Drawing().circle(center=tuple(circle_center), r=circle_radius, stroke = "black", fill="none", stroke_width = 1.0/SCALE )
+    circle_center[1]=MAX_HEIGHT-(float(circle_center[1])+float(circle_ref[1]))
+    svg_entity = svgwrite.Drawing().circle(center=tuple(circle_center), r=circle_radius, stroke = "black", fill="none", stroke_width = 0.5/SCALE )
     #svg_entity.scale(SCALE,-SCALE)
     return svg_entity
 def text(prop):
     #text_insert = [float(x) for x in prop["Position"]["Location"].values()[:2]]
     #text_height = dxf_entity.dxf.height * 1.4 # hotfix - 1.4 to fit svg and dvg
-    svg_entity = svgwrite.Drawing().text(prop["@String"], x=[float(list(prop["Position"]["Location"].values())[0])-float(prop["@Width"])/2],y=[float(list(prop["Position"]["Location"].values())[1])],font_family=prop["@Font"],font_size = float(prop["@Height"])*SCALE)
+    svg_entity = svgwrite.Drawing().text(prop["@String"], x=[float(list(prop["Position"]["Location"].values())[0])-float(prop["@Width"])/2],y=[MAX_HEIGHT-float(list(prop["Position"]["Location"].values())[1])+float(prop["@Height"])/2],font_family=prop["@Font"],font_size = float(prop["@Height"])*SCALE)
     #svg_entity.translate(text_insert[0]*(SCALE), -text_insert[1]*(SCALE))
     return svg_entity
 def polyline(prop,pos=None):
     if pos is None:
         pos={"Location":{"x":0.,"y":0.}}
-    point_list = [(float(list(x.values())[0])+float(list(pos["Location"].values())[0]), float(list(x.values())[1])+float(list(pos["Location"].values())[1])) for x in prop["Coordinate"]]
-    svg_entity = svgwrite.Drawing().polyline(points=point_list, stroke='black', fill='none', stroke_width=1.0/SCALE)
+    point_list = [(float(list(x.values())[0])+float(list(pos["Location"].values())[0]), MAX_HEIGHT-(float(list(x.values())[1])+float(list(pos["Location"].values())[1]))) for x in prop["Coordinate"]]
+    svg_entity = svgwrite.Drawing().polyline(points=point_list, stroke='black', fill='none', stroke_width=0.5/SCALE)
     #svg_entity.scale(SCALE, -SCALE)
     return svg_entity
 def trimmedcurve(prop,pos=None):
@@ -52,29 +52,31 @@ def trimmedcurve(prop,pos=None):
     assert "Circle" in prop
     circle_R=float(prop["Circle"]["@Radius"])
     circle_center=list(prop["Circle"]["Position"]["Location"].values())[:2]
-    if endAngle-startAngle>=360:
-        return svgwrite.Drawing().circle(center=tuple(circle_center), r=circle_R, stroke = "black", fill="none", stroke_width = 1.0/SCALE )
-    isLarge=False
-    if endAngle-startAngle>180: 
-        isLarge=True
     circle_ref=list(pos["Location"].values())[:2]
     circle_center[0]=float(circle_center[0])+float(circle_ref[0])
     circle_center[1]=float(circle_center[1])+float(circle_ref[1])
+    if endAngle-startAngle>=360:
+        circle_center[1]=MAX_HEIGHT-circle_center[1]
+        return svgwrite.Drawing().circle(center=tuple(circle_center), r=circle_R, stroke = "black", fill="none", stroke_width = 0.5/SCALE )
+    isLarge=False
+    if endAngle-startAngle>180: 
+        isLarge=True
+    
     startPoint=[0.,0.]
     startPoint[0]=circle_center[0]+math.cos(startAngle*math.pi/180.0)*circle_R
-    startPoint[1]=circle_center[1]+math.sin(startAngle*math.pi/180.0)*circle_R
+    startPoint[1]=MAX_HEIGHT-(circle_center[1]+math.sin(startAngle*math.pi/180.0)*circle_R)
     endPoint=[0.,0.]
     endPoint[0]=circle_center[0]+math.cos(endAngle*math.pi/180.0)*circle_R
-    endPoint[1]=circle_center[1]+math.sin(endAngle*math.pi/180.0)*circle_R
+    endPoint[1]=MAX_HEIGHT-(circle_center[1]+math.sin(endAngle*math.pi/180.0)*circle_R)
     command=["M",tuple(startPoint)]
-    svg_entity=svgwrite.Drawing().path(d=command,stroke = "black", fill="none", stroke_width = 1.0/SCALE)
+    svg_entity=svgwrite.Drawing().path(d=command,stroke = "black", fill="none", stroke_width = 0.5/SCALE)
     svg_entity.push_arc(target=tuple(endPoint),rotation=0,r=circle_R,large_arc=isLarge,angle_dir="-",absolute=True)
     return svg_entity
 func_dict={"Line":line,"Circle":circle,"text":text,"PolyLine":polyline,"TrimmedCurve":trimmedcurve}
 def Node(circle_center,circle_ref):
     circle_center[0]=float(circle_center[0])+float(circle_ref[0])
-    circle_center[1]=float(circle_center[1])+float(circle_ref[1])
-    svg_entity = svgwrite.Drawing().circle(center=tuple(circle_center), r=1.5, stroke = "black", fill="none", stroke_width = 1.0/SCALE )
+    circle_center[1]=MAX_HEIGHT-(float(circle_center[1])+float(circle_ref[1]))
+    svg_entity = svgwrite.Drawing().circle(center=tuple(circle_center), r=1.5, stroke = "black", fill="none", stroke_width = 0.5/SCALE )
     #svg_entity.scale(SCALE,-SCALE)
     return svg_entity
 def component(prop,shape):
