af0e89bd6885c8390996cd1743231a3ece2d5696
libingye816 <libingye@umich.edu>
2020-01-16 16:01:41 +0800

    finish javascript

--- FrontEnd/myapp/src/Search.js
+++ FrontEnd/myapp/src/Search.js
@@ -1,50 +1,72 @@
 import React from "react";
-import { Form, Dropdown, Button } from "semantic-ui-react";
+import { Form, Dropdown, Button,Container,Segment } from "semantic-ui-react";
 
 class Search extends React.Component {
     constructor(props) {
         super(props);
-        this.URL = 'localhost';
-        this.names = ["SystemUID", "Description", "FullName","Name"];
+        this.URL = 'localhost:8080';
+        this.names = [{ key: "SystemUID", value: "SystemUID", text: "SystemUID" },
+            { key: "Description", value: "Description", text: "Description" },
+            { key: "FullName", value: "FullName", text: "FullName" },
+            {key: "Name", value: "Name", text: "Name" }];
+        this.attribute="";
+        this.condition="";
         this.state = {
-            attribute: ""
-            value:""
-            ifShow: false
-            results:[]
+            ifShow: false,
+            results: [],
+            loading:false
         }
     }
     //componentDidMount() {
-    //    fetch('${this.URL}/sets').then(resp => resp.json()).then(
+    //    fetch(`${this.URL}/sets`).then(resp => resp.json()).then(
     //        data => {this.setState(sets:data) })
     //}
-    handleClick = () => { }
-    handleSelect = () => { }
-    handleChange = () => { }
+    handleClick = (e) => {
+        this.setState({ loading: true });
+        fetch(`${this.URL}/search/${this.attribute}/${this.condition}`).then(resp => resp.json()).then(data => { this.setState({ results: data })})
+        this.setState({ loading: false });
+        this.setState({ ifShow: true });
+    }
+    handleSelect = (e, { value }) => { this.attribute = value;}
+    handleChange = (e, { value }) => { this.condition = value;}
     render() {
-        return {
-            < Form >
-                <Form.Group widths='equal'>
-                
-                    <Form.Select
-                        fluid
-                        label='Attribute Name'
-                        options={this.names}
-                        placeholder='Select an attribute'
-                        onChange={this.handleSelect}
-                    />
-                </Form.Group>
-                <Form.Group widths='equal'>
-                    <Form.Input fluid label='Attribute Value' placeholder='Set the value you want' onChnage={this.handleChange}/>
-                </Form.Group>
-                //<Form.TextArea label='About' placeholder='Tell us more about you...' />
-                <Form.Button onClick={this.handleClick}>Search</Form.Button>
-            </Form >
-            <Segment style={{ overflow: 'auto', maxHeight: 1200 }}>
-            <h1>Search results:</h1>
-            {results.map(result => (
-                <a href={result.url}>{result.name} in {result.file}</a>))}
-            </Segment>
+        let resultShow=null;
+
+        if (this.state.results.length > 0) {
+            resultShow=(
+            
+                this.state.results.map(result => (
+                    <a href={result.url}>{result.name} in {result.file} (ID: {result.ID})</a>))
+            )
         }
+
+        else if (this.state.results.length === 0 && this.state.ifShow) {
+            resultShow=( < p > Find nothing!</p >)
+        }
+        return (
+            <Container>
+                <Form>
+                    <Form.Group widths='equal'>
+
+                        <Form.Select
+                            fluid
+                            label='Attribute Name'
+                            options={this.names}
+                            placeholder='Select an attribute'
+                            onChange={this.handleSelect}
+                        />
+                    </Form.Group>
+                    <Form.Group widths='equal'>
+                        <Form.Input fluid label='Attribute Value' placeholder='Set the value you want' onChange={this.handleChange} />
+                    </Form.Group>
+                    <Form.Button onClick={this.handleClick} loading={this.state.loading}>Search</Form.Button>
+                </Form>
+                <Segment style={{ overflow: 'auto', maxHeight: 1200 }}>
+                    {this.state.ifShow ? <h1>Search results:</h1> : null}
+                    {resultShow}
+                </Segment>
+            </Container>
+        )
     }
 }
 
